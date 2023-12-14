f11e1983cc703a259011219b6728a73aa64d9e1c
clicksomething <sukaramer5@gmail.com>
2023-12-15 01:39:59 +0300

    added person class

--- proj/app/models.py
+++ proj/app/models.py
@@ -1,4 +1,13 @@
-from django.contrib.postgres.fields import ArrayField
 from django.db import models
+gender_choices = [
+    ("F", "Female"),
+    ("M", "Male")
+]
+class person(models.Model):
+    person_id = models.BigIntegerField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length= 100)
+    middle_name = models.CharField(max_length=100)
+    gender=models.CharField(max_length=20,choices=gender_choices)
 
 
