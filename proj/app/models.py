1b27a0db9e27392e53b4c8c1d4db90df4f1bd40f
clicksomething <sukaramer5@gmail.com>
2023-12-15 01:18:40 +0300

    deleted_model

--- proj/app/models.py
+++ proj/app/models.py
@@ -2,11 +2,3 @@ from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 
-class ChessBoard(models.Model):
-    board = ArrayField(
-        ArrayField(
-            models.CharField(max_length=10, blank=True),
-            size=8,
-        ),
-        size=8,
-    )
