cc0160bdaddbc63a29f11291838037c750e79eff
clicksomething <sukaramer5@gmail.com>
2023-12-15 12:32:00 +0300

    Added models
    models added:
    Person, Employee, session.
    
    relations added:
    one to many relation on session to emp

--- proj/app/models.py
+++ proj/app/models.py
@@ -9,5 +9,25 @@ class person(models.Model):
     last_name = models.CharField(max_length= 100)
     middle_name = models.CharField(max_length=100)
     gender=models.CharField(max_length=20,choices=gender_choices)
+    class Meta:
+        abstract = True
+class stranger(person):
+    is_banned = models.BooleanField()
+
+class employee(person):
+    email= models.EmailField()
+    password_encryption = models.CharField(max_length=300)
+    role=models.CharField(max_length=300)
+    last_login=models.DateTimeField()
+    is_logged_in = models.BooleanField()
+
+class session(models.Model):
+    session_id = models.BigIntegerField(primary_key = True)
+    user_id = models.ForeignKey(employee,on_delete=models.CASCADE)
+    login_time=models.DateTimeField()
+    logout_time= models.DateTimeField()
+    IP_address=models.GenericIPAddressField()
+    device_info =models.CharField(max_length=400)
+
 
 
